80fb00f559a52ab9649bb83093651c11b6588424
handapeng <865804820@qq.com>
2022-10-19 10:08:36 +0800

    update

---
+++ src/main/java/com/hdp/algorithm/DetectCycle.java
@@ -0,0 +1,28 @@
+package com.hdp.algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author HDP
+ * @ClassName: DetectCycle
+ * @Description:
+ * @date 2022/10/19 10:07
+ */
+public class DetectCycle {
+    public HasCycle.ListNode detectCycle(HasCycle.ListNode head) {
+        List<HasCycle.ListNode> list = new ArrayList<>();
+        do
+        {
+            if (list.contains(head)) {
+                return head;
+            }
+            list.add(head);
+            if (Objects.nonNull(head)) {
+                head = head.next;
+            }
+        } while (Objects.nonNull(head));
+        return null;
+    }
+}
