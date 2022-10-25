2d43985bbef1ea86015886624fd00d460e677a3b
handapeng <865804820@qq.com>
2022-10-25 16:04:02 +0800

    update

---
+++ src/main/java/com/hdp/juc/UnsafeFactory.java
@@ -0,0 +1,44 @@
+package com.hdp.juc;
+
+import sun.misc.Unsafe;
+
+import java.lang.reflect.Field;
+
+/**
+ * @author HDP
+ * @ClassName: UnsafeFactory
+ * @Description:
+ * @date 2022/10/25 14:45
+ */
+public class UnsafeFactory {
+    /**
+     * 获取Unsafe对象
+     * @return
+     */
+    public static Unsafe getUnsafe() {
+        try {
+            Field theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
+            theUnsafe.setAccessible(true);
+            return (Unsafe) theUnsafe.get(null);
+        } catch (
+                Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * 获取字段的内存偏移量
+     * @param unsafe
+     * @param clazz
+     * @param fieldName
+     * @return
+     */
+    public static long getFieldOffset(Unsafe unsafe, Class clazz, String fieldName) {
+        try {
+            return unsafe.objectFieldOffset(clazz.getField(fieldName));
+        } catch (NoSuchFieldException e) {
+            throw new Error(e);
+        }
+    }
+
+}
--- src/main/java/com/hdp/juc/VisibilityTest.java
+++ src/main/java/com/hdp/juc/VisibilityTest.java
@@ -1,17 +1,74 @@
 package com.hdp.juc;
 
+import java.util.concurrent.locks.LockSupport;
+
 /**
  * @author HDP
  * @ClassName: VisibilityTest
  * @Description:
+ * -XX:+UnlockDiagnosticVMOptions -XX:+PrintAssembly -Xcomp  查看汇编指令
+ * hsdis-amd64.dll
  * @date 2022/10/25 10:33
  */
 public class VisibilityTest {
-
-    private boolean flag = true;
+    //加volatile可以跳出循环 内存屏障
+    private  boolean flag = true;
+    //改为Integer 可以跳出循环 final字段保证可见性 内存屏障
+    // lock addl $0x0,(%rsp)     ;*putstatic finalRefCount
     private Integer count = 0;
 
-    public static void main(String[] args) {
+    public void refresh() {
+        flag = false;
+        System.out.println(Thread.currentThread().getName() + "修改flag：" + flag);
+    }
 
+    //storeLoad  JVM内存屏障 ---》 （汇编层面指令） lock addl $0x0,(%rsp)
+    // lock前缀指令不是内存屏障的指令，但是有内存屏障的效果，修改的值立即载入主内存，让其余共享变量副本的缓存失效
+    public void load() {
+        System.out.println(Thread.currentThread().getName() + "开始执行。。。");
+        while (flag) {
+            count++;
+            //内存屏障
+//            UnsafeFactory.getUnsafe().storeFence();
+            //释放时间片，上下文切换，共享变量的副本失效，加载上下文读取主内存的flag=false
+//            Thread.yield();
+            //synchronized  内存屏障
+//            System.out.println(count);
+            //内存屏障
+//            LockSupport.unpark(Thread.currentThread());
+            //上下文切换
+//            shortWait(10000);
+//            try {
+//                //内存屏障
+//                Thread.sleep(1);
+//            } catch (
+//                    InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+
+            //Java中可见性如何保证？ 方式归类有两种：
+            //1. jvm层面 storeLoad内存屏障  ===》x86  lock替代mfence
+            //2. 上下文切换  Thread.yield();
+
+        }
+        System.out.println(Thread.currentThread().getName() + "跳出循环,count=" + count);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        VisibilityTest test = new VisibilityTest();
+        Thread threadA = new Thread(test::load, "threadA");
+        threadA.start();
+        Thread.sleep(1000);
+        Thread threadB = new Thread(test::refresh, "threadB");
+        threadB.start();
+    }
+
+    public static void shortWait(long interval) {
+        long start = System.nanoTime();
+        long end;
+        do
+        {
+            end = System.nanoTime();
+        } while (start + interval >= end);
     }
 }
