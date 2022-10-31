931878f45a2c128763bb8b0a5088acaaaac330f7
handapeng <865804820@qq.com>
2022-10-31 10:06:16 +0800

    update

--- src/main/java/com/hdp/algorithm/IsPalindrome.java
+++ src/main/java/com/hdp/algorithm/IsPalindrome.java
@@ -1,26 +1,31 @@
 package com.hdp.algorithm;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
  * @author HDP
  * @ClassName: IsPalindrome
- * @Description:
+ * @Description: 回文链表
  * @date 2022/10/28 15:26
  */
 public class IsPalindrome {
     public boolean isPalindrome(HasCycle.ListNode head) {
-        Set<Integer> set = new HashSet<>();
-        if(head.next==null){
-            return true;
-        }
+        List<Integer> list = new ArrayList<>();
         while (head != null) {
-            if (!set.add(head.val)&&set.size()>2&&head.next!=null) {
-                return true;
-            }
+            list.add(head.val);
             head = head.next;
         }
-        return false;
+        int i = 0, j = list.size() - 1;
+        while (i < j) {
+            if (!list.get(i).equals(list.get(j))) {
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
     }
 }
